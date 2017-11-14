464ff8c44917aaa4c3a805d86049bc722c5d9f7f
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-11-14 17:56:20 -0500

    Added Podfeed class to package level.

--- podfeed/__init__.py
+++ podfeed/__init__.py
@@ -1,3 +1,12 @@
 from logging import getLogger, NullHandler
 LOGGER = getLogger('podfeed')
 LOGGER.addHandler(NullHandler())
+
+__all__ = [
+  "crooked",
+  "gimlet",
+  "npr",
+  "other"
+]
+
+from .Podfeed import Podfeed
