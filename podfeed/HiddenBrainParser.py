1ec8c7fbe10172434d3d03dd4cbdf8331cd0fcf1
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-11-13 20:42:20 -0500

    Added Hidden Brain specific extraction and parsing code.

--- podfeed/HiddenBrainParser.py
+++ podfeed/HiddenBrainParser.py
@@ -1,7 +1,29 @@
+from urllib.request import urlopen
+import re
+
 from .AbstractFeedParser import AbstractFeedParser
 
 class HiddenBrainParser(AbstractFeedParser):
   URL = "https://www.npr.org/rss/rss.php?id=423302056"
+  MP3_REGEX_STR = "(https://.*mp3)"
 
   def __init__(self):
     AbstractFeedParser.__init__(self, self.URL)
+    self.mp3_regex = re.compile(self.MP3_REGEX_STR)
+
+  def getMp3File(self, entry):
+    page_link = entry['link']
+    mp3_link = None
+
+    with urlopen(page_link) as page:
+      for line in page:
+        results = self.mp3_regex.search(line.decode())
+
+        if results:
+          mp3_link = results.group(0)
+
+    if mp3_link:
+      return urlopen(mp3_link)
+
+    else:
+      raise Exception("Could not find MP3 link!")
