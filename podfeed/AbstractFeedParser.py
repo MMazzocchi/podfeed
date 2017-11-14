158a0ea73f20f2367bd7252440c8eb384b60c1fd
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-11-13 22:00:31 -0500

    Continued cleanup of AbstractFeedParser.

--- podfeed/AbstractFeedParser.py
+++ podfeed/AbstractFeedParser.py
@@ -19,39 +19,37 @@ class AbstractFeedParser:
       entries = data['entries']
 
       for entry in entries:
-        self.processEntryIfNew(entry, date)
+        if self.isNewEntry(entry, date):
+          self.processEntry(entry)
 
-  def processEntryIfNew(self, entry, date):
+  def isNewEntry(self, entry, date):
     updated = mktime(entry['updated_parsed'])
+    return updated >= date
 
-    if updated >= date:
-      filename = self.makeFilename(entry)
+  def processEntry(self, entry):
+    filename = self.makeFilename(entry)
 
-      try:
-        self.parseAndWrite(entry, filename)
+    try:
+      mp3_link = self.getMp3Link(entry)
+      with urlopen(mp3_link) as response:
+        self.writeResponseToFile(response, filename)
 
-      except Exception as e:
-        print("An error occured while parsing an entry. This entry "+
-          "will be ignored: {0}".format(e))
+    except Exception as e:
+      print("An error occured while parsing an entry. This entry "+
+        "will be ignored: {0}".format(e))
 
   def makeFilename(self, entry):
     time = floor(mktime(entry['updated_parsed']))
     return "{0}/{1}_{2}.mp3".format(self.directory, self.name, time)
 
-  def parseAndWrite(self, entry, filename):
-    mp3_link = self.getMp3Link(entry)
-
-    with urlopen(mp3_link) as response:
-      self.writeResponseToFile(response, filename)
-
   def writeResponseToFile(self, response, filename):
     with open(filename, "wb") as outfile:
-     print("Writing {0}...".format(filename))
+      print("Writing {0}...".format(filename))
 
-     chunk = response.read(16 * 1024)
-     while chunk:
-       outfile.write(chunk)
-       chunk = response.read(16 * 1024)                
+      chunk = response.read(16 * 1024)
+      while chunk:
+        outfile.write(chunk)
+        chunk = response.read(16 * 1024)                
 
   def getMp3Link(self, entry):
     raise NotImplementedError("AbstractFeedParser.getMp3Link() "+
