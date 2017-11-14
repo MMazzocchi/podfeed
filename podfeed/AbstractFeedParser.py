7e8725d5a2075a0da2933789976943fdd190ec96
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-11-13 21:25:17 -0500

    Refactored and cleaned up AbstractFeedParser. Moved directory settign into constructor.

--- podfeed/AbstractFeedParser.py
+++ podfeed/AbstractFeedParser.py
@@ -4,11 +4,12 @@ from urllib.request import urlopen
 from math import floor
 
 class AbstractFeedParser:
-  def __init__(self, name, url):
+  def __init__(self, name, url, directory):
     self.name = name
     self.url = url
+    self.directory = directory
 
-  def saveNewEpisodes(self, date, directory):
+  def saveNewEpisodes(self, date):
     data = feedparser.parse(self.url)
 
     if data['bozo'] == 1:
@@ -18,26 +19,39 @@ class AbstractFeedParser:
       entries = data['entries']
 
       for entry in entries:
-        updated = mktime(entry['updated_parsed'])
+        self.processEntryIfNew(entry, date)
 
-        if updated >= date:
-          try:
-            filename = "{0}/{1}_{2}.mp3".format(
-              directory, self.name, floor(updated))
+  def processEntryIfNew(self, entry, date):
+    updated = mktime(entry['updated_parsed'])
 
-            mp3_link = self.getMp3Link(entry)
-            with urlopen(mp3_link) as response:
-              with open(filename, "wb") as outfile:
-                print("Writing {0}...".format(filename))
+    if updated >= date:
+      filename = self.makeFilename(entry)
 
-                chunk = response.read(16 * 1024)
-                while chunk:
-                  outfile.write(chunk)
-                  chunk = response.read(16 * 1024)                
+      try:
+        self.parseAndWrite(entry, filename)
 
-          except Exception as e:
-            print("An error occured while parsing an entry. This entry "+
-              "will be ignored: {0}".format(e))
+      except Exception as e:
+        print("An error occured while parsing an entry. This entry "+
+          "will be ignored: {0}".format(e))
+
+  def makeFilename(self, entry):
+    time = floor(mktime(entry['updated_parsed']))
+    return "{0}/{1}_{2}.mp3".format(self.directory, self.name, time)
+
+  def parseAndWrite(self, entry, filename):
+    mp3_link = self.getMp3Link(entry)
+
+    with urlopen(mp3_link) as response:
+      self.writeResponseToFile(response, filename)
+
+  def writeResponseToFile(self, response, filename):
+    with open(filename, "wb") as outfile:
+     print("Writing {0}...".format(filename))
+
+     chunk = response.read(16 * 1024)
+     while chunk:
+       outfile.write(chunk)
+       chunk = response.read(16 * 1024)                
 
   def getMp3Link(self, entry):
     raise NotImplementedError("AbstractFeedParser.getMp3Link() "+
