3759e44b1f81e68e788c972c301e08c9294f19dd
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-11-13 19:09:23 -0500

    Added basic methods to Reader.

--- podfeed/Reader.py
+++ podfeed/Reader.py
@@ -1,2 +1,17 @@
+import feedparser
+
 class Reader:
-  pass
+  def __init__(self):
+    self.urls = []
+
+  def addUrl(self, url):
+    self.urls.append(url)
+
+  def addUrls(self, urls):
+    self.urls.extend(urls)
+
+  def clearUrls(self):
+    self.urls.clear()
+
+  def getFilesSince(self, date):
+    pass 
