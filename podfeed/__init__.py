10fd6b0d44e49272bac7f33b93cc5549535c0d2b
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-11-14 18:24:36 -0500

    Removed __all__ from __init__.py.

--- podfeed/__init__.py
+++ podfeed/__init__.py
@@ -2,11 +2,4 @@ from logging import getLogger, NullHandler
 LOGGER = getLogger('podfeed')
 LOGGER.addHandler(NullHandler())
 
-__all__ = [
-  "crooked",
-  "gimlet",
-  "npr",
-  "other"
-]
-
 from .Podfeed import Podfeed
