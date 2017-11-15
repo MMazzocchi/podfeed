431317acd3c1e2c98540bab75ab53f29a51bc025
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-11-15 17:18:53 -0500

    Added AndThatsWhyWeDrinkParser.

--- podfeed/other.py
+++ podfeed/other.py
@@ -13,3 +13,14 @@ class IntelligenceMattersParser(AbstractFeedParser):
 
   def getMp3Link(self, entry):
     return entry.links[1].href
+
+class AndThatsWhyWeDrinkParser(AbstractFeedParser):
+  ''' Parser for the And That's Why We Drink podcast '''
+  NAME = "and_thats_why_we_drink"
+  URL = "https://audioboom.com/channels/4911419.rss"
+
+  def __init__(self):
+    AbstractFeedParser.__init__(self, self.NAME, self.URL)
+
+  def getMp3Link(self, entry):
+    return entry.media_content[0]['url']
