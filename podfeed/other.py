407c24f668bba05522de9b1f801547e606991fdb
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2017-11-16 20:44:44 -0500

    Added Revisionist History, Freakonomics Radio, Criminal, and Myths and Legends.

--- podfeed/other.py
+++ podfeed/other.py
@@ -40,7 +40,42 @@ class GeointerestingParser(AbstractFeedParser):
   URL = "http://feeds.soundcloud.com/users/soundcloud:users:149471698/sounds.rss"
 
   def __init__(self):
-    FeedBurnerParser.__init__(self, self.NAME, self.URL)
+    AbstractFeedParser.__init__(self, self.NAME, self.URL)
 
   def getMp3Link(self, entry):
     return entry.links[1].href
+
+class RevisionistHistoryParser(FeedBurnerParser):
+  ''' Parser for the Revisionist History podcast '''
+  NAME = "revisionist_history"
+  URL = "http://feeds.feedburner.com/RevisionistHistory"
+
+  def __init__(self):
+    FeedBurnerParser.__init__(self, self.NAME, self.URL)
+
+class FreakonomicsRadioParser(FeedBurnerParser):
+  ''' Parser for the Freakonomics Radio podcast '''
+  NAME = "freakonomics_radio"
+  URL = "http://feeds.feedburner.com/freakonomicsradio"
+
+  def __init__(self):
+    FeedBurnerParser.__init__(self, self.NAME, self.URL)
+
+class CriminalParser(FeedBurnerParser):
+  ''' Parser for the Criminal podcast '''
+  NAME = "criminal"
+  URL = "http://feeds.thisiscriminal.com/CriminalShow"
+
+  def __init__(self):
+    FeedBurnerParser.__init__(self, self.NAME, self.URL)
+
+class MythsAndLegendsParser(AbstractFeedParser):
+  ''' Parser for the Myths and Legends podcast '''
+  NAME = "myths_and_lengends"
+  URL = "http://mythpodcast.libsyn.com/rss"
+
+  def __init__(self):
+    AbstractFeedParser.__init__(self, self.NAME, self.URL)
+
+  def getMp3Link(self, entry):
+    return entry.links[0].href
