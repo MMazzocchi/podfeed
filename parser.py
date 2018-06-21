34dc9e366462d19e978ab1604a0b9521224384bb
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-06-21 15:00:31 -0400

    Changed method names to track from MP3

--- parser.py
+++ parser.py
@@ -9,8 +9,8 @@ LOGGER = getLogger('podfeed')
 
 TRACK_REGEX = re.compile(r'^.*\.(mp3|wav|aif.?|flac|ogg|wma)$')
 
-def validMp3Link(link):
-  ''' Return true if the link appears to be a valid MP3 link. '''
+def validTrackLink(link):
+  ''' Return true if the link appears to be a valid link. '''
   filename = basename(link).split("?")[0]
   return TRACK_REGEX.match(filename)
 
@@ -64,8 +64,9 @@ class Episode:
 
 class StandardFeedParser:
   ''' StandardFeedParser is a base class used to parse an RSS feed and identify,
-      extract, and download MP3 files from it. It will download a feed, parse
-      new entries from it, and return those entries packaged in Episode objects.
+      extract, and download episode files from it. It will download a feed,
+      parse new entries from it, and return those entries packaged in Episode
+      objects.
   '''
   CHUNK_SIZE = 32*1024
 
@@ -104,23 +105,23 @@ class StandardFeedParser:
   def makeEpisode(self, title, entry):
     ''' Create an Episode object for this entry '''
     time = floor(mktime(entry['updated_parsed']))
-    link = self.getMp3Link(entry)
+    link = self.getTrackLink(entry)
 
     episode = Episode(title, time, link)
     return episode
 
-  def getMp3Link(self, entry):
+  def getTrackLink(self, entry):
     ''' Extract a link to an MP3 file from this entry. By default, this method
     searches the "links" section of an entry for one that looks like an MP3.
     If none is found, returns None. '''
-    mp3_link = None
+    track_link = None
 
     if ('links' in entry) and (len(entry.links) > 0):
       for link in entry.links:
-        if ('href' in link) and validMp3Link(link.href):
-          mp3_link = link.href
+        if ('href' in link) and validTrackLink(link.href):
+          track_link = link.href
 
-    return mp3_link
+    return track_link
 
 def parseFeed(url, date, parser=StandardFeedParser):
   ''' Parses the feed given by <url>, and returns episodes posted after <date>.
