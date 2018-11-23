ea462148796820e488742c460b63537fc0675c1a
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-11-23 08:33:55 -0500

    Bubbled up classes to package level.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,7 @@
 from logging import getLogger, NullHandler
 LOGGER = getLogger('podfeed')
 LOGGER.addHandler(NullHandler())
+
+from .parser import StandardFeedParser
+from .parser import parseFeed
+from .playlists import Playlist
