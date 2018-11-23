51ac1faa998265ed8fbd09a01da6c8c937a918bc
Max Mazzocchi <maxwell.mazzocchi@gmail.com>
2018-11-23 08:43:26 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="podfeed",
+  version="1.0.0",
+  author="Max Mazzocchi",
+  author_email="maxwell.mazzocchi@gmail.com",
+  description="A simple podcast feed parser",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/mmazzocchi/podfeed",
+  packages=setuptools.find_packages(),
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+)
